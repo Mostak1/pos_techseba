3a64811400697b6272809a73e32c83c9fa538120
Mostak1 <mostakidb@gmail.com>
2026-06-03 16:11:02 +0600

    Define STDIN, STDOUT, STDERR constants in clear-cache route

--- routes/web.php
+++ routes/web.php
@@ -74,6 +74,16 @@ use Illuminate\Support\Facades\Route;
 
 include_once 'install_r.php';
 Route::get('/clear-cache', function () {
+    if (!defined('STDIN')) {
+        define('STDIN', fopen('php://stdin', 'r'));
+    }
+    if (!defined('STDOUT')) {
+        define('STDOUT', fopen('php://stdout', 'w'));
+    }
+    if (!defined('STDERR')) {
+        define('STDERR', fopen('php://stderr', 'w'));
+    }
+
     // Ensure storage folders exist and are writeable
     $storage_dirs = [
         storage_path('framework/sessions'),
