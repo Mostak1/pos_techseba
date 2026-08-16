32bb4696548a17563e0dfe676c17a98d8d5f8304
Mostak1 <mostakidb@gmail.com>
2026-08-16 22:27:58 +0600

    allow all business admin roles to access business settings

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -26,8 +26,7 @@ class AuthServiceProvider extends ServiceProvider
         $this->registerPolicies();
 
         Gate::before(function ($user, $ability) {
-            if (in_array($ability, ['backup', 'superadmin',
-                'business_settings.access', 'manage_modules', ])) {
+            if (in_array($ability, ['backup', 'superadmin', 'manage_modules'])) {
                 $administrator_list = config('constants.administrator_usernames');
 
                 if (in_array(strtolower($user->username), explode(',', strtolower($administrator_list)))) {
